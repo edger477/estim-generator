fd2e552bbcdb7a656b51c03f8621da5bc0e8a208
edger477 <edgingforever@outlook.com>
2023-04-10 20:38:29 +0200

    added freq modulation

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ let app = new Vue({
         amp: 0.5,
         mod: 0,
         modamp: 10,
+        freq_mod: 0,
+        freq_mod_amp: 0,
         carrier: null,
-        modulator: null,
+        amp_modulator: null,
         p: null,
         p5: null
     },
@@ -27,27 +29,35 @@ let app = new Vue({
                 const freq = parseFloat(self.freq);
                 const amp = parseFloat(self.amp);
                 const mod = parseFloat(self.mod);
-                const modamp = parseFloat(self.modamp/100);
+                const modamp = parseFloat(self.modamp / 100);
+                const freqmod = parseFloat(self.freq_mod);
+                const freqmodamp = parseFloat(self.freq_mod_amp);
                 self.carrier.freq(freq);
                 self.carrier.amp(amp);
                 // console. log(modulator.scale(-1, 1, -10, 10));
-                self.modulator.freq(mod);
-                self.modulator.amp(modamp);
-                // self.modulator.scale(-1, 1, -1, 1);
-                // self.carrier.freq(freq + self.modulator.scale(-1, 1, -10, 10));
-                // self.carrier.amp(self.modulator.scale(-1, 1, 0, 1));
+                self.amp_modulator.freq(mod);
+                self.amp_modulator.amp(modamp);
+                self.freq_modulator.freq(freqmod);
+                self.freq_modulator.amp(freqmodamp);
+                // self.amp_modulator.scale(-1, 1, -1, 1);
+                // self.carrier.freq(freq + self.amp_modulator.scale(-1, 1, -10, 10));
+                // self.carrier.amp(self.amp_modulator.scale(-1, 1, 0, 1));
             };
             self.carrier = new p5.Oscillator();
             self.carrier.setType("sine");
-            self.modulator = new p5.Oscillator('sine');
-            self.modulator.disconnect();
-            self.carrier.amp(self.modulator.scale(-1, 1, -1, 1));
-            // self.modulator.setType('sine');
+            self.amp_modulator = new p5.Oscillator('sine');
+            self.amp_modulator.disconnect();
+            self.freq_modulator = new p5.Oscillator('sine');
+            self.freq_modulator.disconnect();
+            self.carrier.amp(self.amp_modulator);
+            self.carrier.freq(self.freq_modulator);
+            // self.amp_modulator.setType('sine');
         },
         playSound() {
             this.carrier.start();
             this.carrier.amp(1.0);
-            this.modulator.start();
+            this.amp_modulator.start();
+            this.freq_modulator.start();
             // setTimeout(() => {
             //     this.carrier.amp(0.0);
             // }, 1000);
@@ -55,7 +65,8 @@ let app = new Vue({
         stopSound() {
             this.carrier.stop();
             this.carrier.amp(0);
-            this.modulator.stop();
+            this.amp_modulator.stop();
+            this.freq_modulator.stop();
         },
         saveSound() {
             const filename = `sound_${this.freq.toFixed(0)}_${this.amp.toFixed(2)}_${this.mod.toFixed(0)}.mp3`;
